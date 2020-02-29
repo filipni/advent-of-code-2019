bb9c8d8333fbc4ec3aa58b007c22412aaec08db1
filipni <filip.nilsson+github@protonmail.com>
2020-02-29 18:26:21 +0100

    Add project files.

---
+++ AdventOfCode/IntcodeComputer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class IntcodeComputer
+    {
+        private int pc;
+        private int[] memory;
+        private bool halted;
+
+        public IntcodeComputer(int[] memory) => this.memory = memory;
+
+        public void Run()
+        {
+            while (!halted)
+            {
+                int instructionLength = ExecuteInstruction();
+                pc += instructionLength;
+            }
+        }
+
+        private int ExecuteInstruction()
+        {
+            int instructionFormat = memory[pc];
+            int opcode = GetDigits(instructionFormat, 0, 2);
+            int paramsFormat = GetDigits(instructionFormat, 2, 3);
+            List<int> paramPositions;
+
+            switch (opcode)
+            {
+                case 1:
+                    paramPositions = GetParamPositions(paramsFormat, 3);
+                    return ADD(memory[paramPositions[0]], memory[paramPositions[1]], out memory[paramPositions[2]]);
+                case 2:
+                    paramPositions = GetParamPositions(paramsFormat, 3);
+                    return MUL(memory[paramPositions[0]], memory[paramPositions[1]], out memory[paramPositions[2]]);
+                case 3:
+                    paramPositions = GetParamPositions(paramsFormat, 1);
+                    return IN(out memory[paramPositions[0]]);
+                case 4:
+                    paramPositions = GetParamPositions(paramsFormat, 1);
+                    return OUT(memory[paramPositions[0]]);
+                case 5:
+                    paramPositions = GetParamPositions(paramsFormat, 2);
+                    return JT(memory[paramPositions[0]], memory[paramPositions[1]]);
+                case 6:
+                    paramPositions = GetParamPositions(paramsFormat, 2);
+                    return JF(memory[paramPositions[0]], memory[paramPositions[1]]);
+                case 7:
+                    paramPositions = GetParamPositions(paramsFormat, 3);
+                    return LET(memory[paramPositions[0]], memory[paramPositions[1]], out memory[paramPositions[2]]);
+                case 8:
+                    paramPositions = GetParamPositions(paramsFormat, 3);
+                    return EQ(memory[paramPositions[0]], memory[paramPositions[1]], out memory[paramPositions[2]]);
+                case 99:
+                    return HLT();
+                default:
+                    Console.WriteLine("Unknown opcode: {0}", opcode);
+                    return 1;
+            }
+        }
+
+        private List<int> GetParamPositions(int paramsFormat, int numParams)
+        {
+            var indices = new List<int>();
+
+            for (int i = 0; i < numParams; i++)
+            {
+                bool immediateMode = GetDigits(paramsFormat, i, 1) == 1;
+                int index;
+
+                if (immediateMode)
+                    index = pc + 1 + i;
+                else
+                    index = memory[pc + 1 + i];
+
+                indices.Add(index);
+            }
+
+            return indices;
+        }
+
+        private int GetDigits(int num, int start, int length)
+        {
+            start = (int)Math.Pow(10, start);
+            length = (int)Math.Pow(10, length);
+            return num / start % length;
+        }
+
+        private int ADD(int a, int b, out int output)
+        {
+            output = a + b;
+            return 4;
+        }
+
+        private int MUL(int a, int b, out int output)
+        {
+            output = a * b;
+            return 4;
+        }
+
+        private int IN(out int output)
+        {
+            Console.Write("Input: "); 
+            output = int.Parse(Console.ReadLine());
+            return 2;
+        }
+
+        private int OUT(int a)
+        {
+            Console.WriteLine("Output: {0}", a);
+            return 2;
+        }
+
+        private int JT(int a, int b)
+        {
+            if (a != 0)
+            {
+                pc = b;
+                return 0;
+            }
+            return 3;
+        }
+
+        private int JF(int a, int b)
+        {
+            if (a == 0)
+            {
+                pc = b;
+                return 0;
+            }
+            return 3;
+        }
+
+        private int LET(int a, int b, out int output)
+        {
+            if (a < b)
+                output = 1;
+            else 
+                output = 0;
+            return 4;
+        }
+
+        private int EQ(int a, int b, out int output)
+        {
+            if (a == b)
+                output = 1;
+            else
+                output = 0;
+            return 4;
+        }
+
+        private int HLT()
+        {
+            halted = true;
+            return 1;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        const string INPUT_DIR = @"C:\Users\filip\source\repos\AdventOfCode\AdventOfCode\input";
+
+        static int[] GetInput(string filename, string delimiter)
+        {
+            string path     = Path.Combine(INPUT_DIR, filename);
+            string text     = File.ReadAllText(path);
+            var input       = text.Split(delimiter).Where(x => double.TryParse(x, out _));
+            var parsedInput = input.Select(x => int.Parse(x));
+
+            return parsedInput.ToArray();
+        }
+        
+        static int Day1Part1() => GetInput("day1.txt", "\n").Select(x => x / 3 - 2).Sum();
+
+        static void Day1Part2()
+        {
+            var input = new Queue<int>(GetInput("day1.txt", "\n"));
+            int sum = 0;
+
+            while (input.Count != 0)
+            {
+                int module = input.Dequeue();
+                int fuel = module / 3 - 2;
+                if (fuel > 0)
+                {
+                    sum += fuel;
+                    input.Enqueue(fuel);
+                }
+            }
+
+            Console.WriteLine("Sum: {0}", sum);
+        }
+
+        static void Day2Part1()
+        {
+            int[] data = GetInput("day2.txt", ",");
+            Console.WriteLine("Output: {0}", RunProgram(12, 2, data));
+        }
+
+        static void Day2Part2()
+        {
+            int[] data = GetInput("day2.txt", ",");
+
+            var outputs = from x in Enumerable.Range(0, 100)
+                          from y in Enumerable.Range(0, 100)
+                          where RunProgram(x, y, (int[])data.Clone()) == 19690720
+                          select 100 * x + y;
+
+            Console.WriteLine("Output: {0}", outputs.FirstOrDefault());
+        }
+
+        static int RunProgram(int noun, int verb, int[] memory)
+        {
+            memory[1] = noun;
+            memory[2] = verb;
+
+            for (int i = 0; i < memory.Length; i += 4)
+            {
+                int opcode = memory[i];
+                int input1Index = memory[i + 1];
+                int input2Index = memory[i + 2];
+                int outputIndex = memory[i + 3];
+
+                switch (opcode)
+                {
+                    case 1:
+                        memory[outputIndex] = memory[input1Index] + memory[input2Index];
+                        break;
+                    case 2:
+                        memory[outputIndex] = memory[input1Index] * memory[input2Index];
+                        break;
+                    case 99:
+                        return memory[0];
+                    default:
+                        Console.WriteLine("Unknown opcode: {0}", opcode);
+                        return -1;
+                }
+            }
+
+            Console.WriteLine("Program was never halted");
+            return -1;
+        }
+
+        static void Day3()
+        {
+            string filepath = Path.Combine(INPUT_DIR, "day3.txt");
+            string text = System.IO.File.ReadAllText(filepath);
+            string[] inputs = text.Split("\n");
+
+            var wire1Directions = ParseDay3Input(inputs[0]);
+            var wire2Directions = ParseDay3Input(inputs[1]);
+
+            var wire1Path = CreateWirePath(wire1Directions);
+            var wire2Path = CreateWirePath(wire2Directions);
+
+            var intersections = wire1Path.Where(x => wire2Path.Contains(x));
+
+            int distanceClosestIntersect = intersections.Select(x => Math.Abs(x.Item1) + Math.Abs(x.Item2)).Min();
+            Console.WriteLine("Distance to closest intersection: {0}", distanceClosestIntersect);
+
+            int minSteps = intersections.Select(x => wire1Path.IndexOf(x) + wire2Path.IndexOf(x) + 2).Min(); // Add 2 to compensate for zero based indexing 
+            Console.WriteLine("Fewest steps to intersection: {0}", minSteps);
+        }
+
+        static List<(char, int)> ParseDay3Input(string input)
+        {
+            var matches = Regex.Matches(input, @"([UDLR])(\d+)");
+            var parsedInputs = new List<(char, int)>();
+
+            foreach (Match m in matches)
+            {
+                char direction = char.Parse(m.Groups[1].Value);
+                int steps = int.Parse(m.Groups[2].Value);
+                parsedInputs.Add((direction, steps)); 
+            }
+
+            return parsedInputs;
+        }
+
+        static List<(int, int)> CreateWirePath(List<(char, int)> directions)
+        {
+            var currentPos = (X: 0, Y: 0);
+            var path = new List<(int, int)>();
+
+            foreach ((char direction, int steps) in directions)
+            {
+                for (int i = 1; i <= steps; i++)
+                {
+                    switch (direction)
+                    {
+                        case 'U':
+                            currentPos.Y += 1;
+                            break;
+                        case 'D':
+                            currentPos.Y -= 1;
+                            break;
+                        case 'L':
+                            currentPos.X -= 1;
+                            break;
+                        case 'R':
+                            currentPos.X += 1;
+                            break;
+                    }
+                    path.Add(currentPos);
+                }
+            }
+
+            return path;
+        }
+
+        static void Day4()
+        {
+            Console.WriteLine("Number of different passwords");
+            int numPasswords = GetNumberOfPasswords(x => x >= 2);
+            Console.WriteLine("Part1: {0}", numPasswords);
+            numPasswords = GetNumberOfPasswords(x => x == 2);
+            Console.WriteLine("Part2: {0}", numPasswords);
+        }
+
+        static int GetNumberOfPasswords(Func<int, bool> streakFilter)
+        {
+            int numPasswords = 0;
+
+            for (int password = 125730; password <= 579381; password++)
+            {
+                bool isIncreasing = true;
+                var streaks = new List<int>();
+                var currentStreak = 0;
+                char previousDigit = ' ';
+
+                foreach (char digit in password.ToString())
+                {
+                    if (digit == previousDigit)
+                        currentStreak++;
+                    else
+                    {
+                        streaks.Add(currentStreak);
+                        currentStreak = 1;
+                    }
+
+                    if (digit < previousDigit)
+                    {
+                        isIncreasing = false;
+                        break;
+                    }
+
+                    previousDigit = digit;
+                }
+
+                streaks.Add(currentStreak);
+                bool hasDoubleDigit = streaks.Where(streakFilter).Any();
+
+                if (hasDoubleDigit && isIncreasing)
+                    numPasswords++;
+            }
+
+            return numPasswords;
+        }
+
+        static void Day5()
+        {
+            int[] data = GetInput("day5.txt", ",");
+            IntcodeComputer computer = new IntcodeComputer(data);
+            computer.Run();
+        }
+
+        static void Main() => Day5();
+    }
+}
